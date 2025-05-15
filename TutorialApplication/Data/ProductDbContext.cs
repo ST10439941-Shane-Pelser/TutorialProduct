7fbaa00b23aff1f99cf5b3b08ab7fe753c97627b
Shane Pelser <shanejpelser.dev@gmail.com>
2025-05-15 14:41:20 +0200

    Complete Ice Task 3

---
+++ TutorialApplication/Data/ProductDbContext.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using TutorialApplication.Models;
+
+namespace TutorialApplication.Data;
+
+public partial class ProductDbContext : DbContext
+{
+    public ProductDbContext()
+    {
+    }
+
+    public ProductDbContext(DbContextOptions<ProductDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.HasKey(e => e.ProductId).HasName("PK__products__47027DF5A44B6576");
+
+            entity.ToTable("products");
+
+            entity.Property(e => e.ProductId).HasColumnName("product_id");
+            entity.Property(e => e.Category)
+                .HasMaxLength(100)
+                .IsUnicode(false)
+                .HasColumnName("category");
+            entity.Property(e => e.Description)
+                .HasColumnType("text")
+                .HasColumnName("description");
+            entity.Property(e => e.Name)
+                .HasMaxLength(255)
+                .IsUnicode(false)
+                .HasColumnName("name");
+            entity.Property(e => e.Price)
+                .HasColumnType("decimal(10, 2)")
+                .HasColumnName("price");
+            entity.Property(e => e.QuantityInStock)
+                .HasDefaultValue(0)
+                .HasColumnName("quantity_in_stock");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ TutorialApplication/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TutorialApplication.Models;
+
+public partial class Product
+{
+    public int ProductId { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string? Description { get; set; }
+
+    public decimal Price { get; set; }
+
+    public int? QuantityInStock { get; set; }
+
+    public string? Category { get; set; }
+}
